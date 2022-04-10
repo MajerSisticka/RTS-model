0e3be8b37f67403ff22ca2011efc4d888a2dcc5c
SISKA <jakub.majer@mti.cz>
2022-04-10 11:05:26 +0200

    pozice

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -15,28 +15,73 @@ using UnityEngine;
         void Update()
         {
             Vector3 pos = transform.position;
+            
             if ((Input.GetKey("w") || Input.mousePosition.y >= Screen.height - CamBorderMouse))
             {
                 pos.z += CamSpeed * Time.deltaTime;
+                Debug.Log(
+                "pozice Z"
+                + pos.z
+                );
             }
             if ((Input.GetKey("s") || Input.mousePosition.y <= CamBorderMouse))
             {
                 pos.z -= CamSpeed * Time.deltaTime;
+                Debug.Log(
+                "pozice Z"
+                + pos.z
+                );
             }
             if ((Input.GetKey("d") || Input.mousePosition.x >= Screen.width - CamBorderMouse))
             {
                 pos.x += CamSpeed * Time.deltaTime;
+                Debug.Log(
+                "pozice X" +
+                pos.x
+                );
             }
             if ((Input.GetKey("a") || Input.mousePosition.x <= CamBorderMouse))
             {
                 pos.x -= CamSpeed * Time.deltaTime;
+                Debug.Log(
+                "pozice X" +
+                pos.x
+                );
             }
             pos.x = Mathf.Clamp(pos.x, -CamLimit.x, CamLimit.x);
+            Debug.Log(
+            "pozice X" +
+            pos.x
+            );
             pos.z = Mathf.Clamp(pos.z, -CamLimit.y, CamLimit.y);
+            Debug.Log(
+            "pozice Z" +
+            pos.z
+            );
             float MouseScroll = Input.GetAxis("Mouse ScrollWheel");
             pos.y -= MouseScroll * MouseScrollSpeed * 100f * Time.deltaTime;
+            Debug.Log(
+            "pozice Y" +
+            pos.y
+            );
             pos.y = Mathf.Clamp(pos.y, MinY,MaxY);
+            Debug.Log(
+            "pozice Y" +
+            pos.y
+            );
             transform.position = pos;
+            Debug.Log(
+                "pozice Z" 
+                + pos.z
+                );
+            Debug.Log(
+                "pozice Y" +
+                pos.y
+                ); 
+            Debug.Log(
+                "pozice X" +
+                pos.x
+                );
 
         }
     }
